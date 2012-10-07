ccc0981593e57c73b6d6dc5070a60bd554027b6c
root <root@static.(none)>
2012-10-07 17:11:06 -0500

    Wrote post on S3 publisher. Added a spinner to search doohicky. Including S3 publisher

--- content/media/js/search.js
+++ content/media/js/search.js
@@ -1,9 +1,10 @@
 $(document).ready(function(){
     $('#searchsubmit').live('click touchstart', function(e){
         e.preventDefault();
+        $('#search-results').spin();
         $('.result').remove();
         $.getJSON('http://static-eric-search.appspot.com/?query='+$('#s').val()+'&callback=?', function(data){
-            console.log(data);
+            $('#search-results').spin(false);
             var info = '';
             if (data['resp'].length){
                 $.each(data['resp'], function(key,val){
