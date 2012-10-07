29d55725dd4684ee4c5d0bd9f935edf64e0a0947
root <root@static.(none)>
2012-10-07 08:45:48 -0500

    Ready for S3

---
+++ content/media/js/search.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $('#searchsubmit').live('click touchstart', function(e){
+        e.preventDefault();
+        $('.result').remove();
+        $.getJSON('http://static-eric-search.appspot.com/?query='+$('#s').val()+'&callback=?', function(data){
+            console.log(data);
+            var info = '';
+            if (data['resp'].length){
+                $.each(data['resp'], function(key,val){
+                    info += '<div class="result"><h5 class="post-title"><a href="' + val['link'] + '">' + val['title'] + '</a></h5>';
+                    info += '<p><em>Updated: ' + val['updated'] + '</em></p>';
+                    info += '<div class="result-summary" style="display:none;"><p>' + val['summary'] + '</p></div></div>'
+                });
+            } else {
+                info += '<div class"result"><h5 class="post-title">Your search found nothing. Try again</h4></div>';
+            }
+            $('#search-results').append(info);
+        });
+    });
+});
